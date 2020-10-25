1edcae0d5faf978a8f85fe1819ca846c46363b4d
Kingsley Kuan <kingsley@kingsleykuan.com>
2020-10-26 02:26:47 +0800

    Modify FuN model variable names to be more verbose

--- fun/fun_model.py
+++ fun/fun_model.py
@@ -43,19 +43,19 @@ class ManagerModule(nn.Module):
         self.fc_m_space = nn.Linear(num_features_d, num_features_d)
         self.m_rnn = nn.LSTM(num_features_d, num_features_d, batch_first=True)
 
-        self.vf = nn.Linear(num_features_d, 1)
+        self.critic = nn.Linear(num_features_d, 1)
 
     def forward(self, z, state):
         # Input has shape [Batch, Time, Features]
-        s = F.relu(self.fc_m_space(z))
-        g, [h, c] = self.m_rnn(s,
-            [torch.unsqueeze(state[0], 0),
-             torch.unsqueeze(state[1], 0)])
-        g = g / g.norm(dim=-1, keepdim=True)
-        return s, g, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+        latent_state = F.relu(self.fc_m_space(z))
+
+        goal, [h, c] = self.m_rnn(latent_state,
+            [torch.unsqueeze(state[0], 0), torch.unsqueeze(state[1], 0)])
+        goal = goal / goal.norm(dim=-1, keepdim=True)
+        return latent_state, goal, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
     
     def value_function(self, z):
-        return self.vf(z)
+        return self.critic(z)
 
 class WorkerModule(nn.Module):
     """
@@ -71,23 +71,29 @@ class WorkerModule(nn.Module):
         self.w_rnn = nn.LSTM(
             num_features_d, num_features_k * num_actions, batch_first=True)
 
-        self.vf = nn.Linear(num_features_d, 1)
+        self.critic = nn.Linear(num_features_d, 1)
 
-    def forward(self, z, g, state):
-        w = torch.unsqueeze(self.phi(g), -1)
+    def forward(self, z, goal, state):
+        # Input has shape [Batch, Time, Features]
+        goal_embedding = torch.unsqueeze(self.phi(goal), -1)
 
-        u, [h, c] = self.w_rnn(z,
-            [torch.unsqueeze(state[0], 0),
-             torch.unsqueeze(state[1], 0)])
-        u = torch.reshape(
-            u, [u.shape[0], u.shape[1], self.num_actions, self.num_features_k])
+        embedding_matrix, [h, c] = self.w_rnn(z,
+            [torch.unsqueeze(state[0], 0), torch.unsqueeze(state[1], 0)])
+        embedding_matrix = torch.reshape(
+            embedding_matrix, [
+                embedding_matrix.shape[0],
+                embedding_matrix.shape[1],
+                self.num_actions,
+                self.num_features_k
+            ])
 
-        action = torch.squeeze(torch.matmul(u, w), -1)
+        action = torch.squeeze(
+            torch.matmul(embedding_matrix, goal_embedding), -1)
         
         return action, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
 
     def value_function(self, z):
-        return self.vf(z)
+        return self.critic(z)
 
 # TODO: Dilated LSTM states
 # TODO: Pool g over horizon
@@ -110,8 +116,8 @@ class FuNModel(RecurrentNetwork, nn.Module):
         self.z = None
         
         self.manager = ManagerModule(self.num_features_d)
-        self.s = None
-        self.g = None
+        self.latent_state = None
+        self.goal = None
 
         self.worker = WorkerModule(
             self.num_features_d, self.num_features_k, self.num_outputs)
@@ -128,11 +134,11 @@ class FuNModel(RecurrentNetwork, nn.Module):
     def forward_rnn(self, inputs, state, seq_lens):
         self.z = inputs
 
-        s, g, [m_h, m_c] = self.manager(inputs, state[:2])
-        self.s = s
-        self.g = g
+        latent_state, goal, [m_h, m_c] = self.manager(inputs, state[:2])
+        self.latent_state = latent_state
+        self.goal = goal
 
-        action, [w_h, w_c] = self.worker(inputs, g.detach(), state[2:])
+        action, [w_h, w_c] = self.worker(inputs, goal.detach(), state[2:])
 
         return action, [m_h, m_c, w_h, w_c]
 
@@ -157,6 +163,6 @@ class FuNModel(RecurrentNetwork, nn.Module):
         return h
     
     def manager_features(self):
-        assert self.s is not None, "must call forward() first"
-        assert self.g is not None, "must call forward() first"
-        return self.s, self.g
+        assert self.latent_state is not None, "must call forward() first"
+        assert self.goal is not None, "must call forward() first"
+        return self.latent_state, self.goal
