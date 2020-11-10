e2c96cb28b528c8a4b94ed1d895885cdca275724
Sun Yitao <sunyitao7@gmail.com>
2020-11-10 08:48:57 +0800

    update model

--- exploration/small_model.py
+++ exploration/small_model.py
@@ -24,7 +24,7 @@ class SmallConvNet(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
         return x
 
 class SmallConvModel(TorchModelV2, nn.Module):
