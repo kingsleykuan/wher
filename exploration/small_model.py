e5951a557051da63196a206eb7d7a521713f435b
Sun Yitao <sunyitao7@gmail.com>
2020-11-10 08:45:01 +0800

    update model

--- exploration/small_model.py
+++ exploration/small_model.py
@@ -11,19 +11,20 @@ class SmallConvNet(nn.Module):
     """
     def __init__(self):
         super(SmallConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(4, 32, 3, 1, 1)
-        self.conv2 = nn.Conv2d(32, 32, 3, 1, 1)
-        self.conv3 = nn.Conv2d(32, 32, 3, 1, 1)
-        self.conv4 = nn.Conv2d(32, 32, 3, 1, 1)
-        self.fc1 = nn.Linear(512, 256)
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(7 * 7 * 64, 512)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = x.reshape((-1, 512))
-        x = F.relu(self.fc1(x))
+        x = x.float() / 255
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
         return x
 
 class SmallConvModel(TorchModelV2, nn.Module):
